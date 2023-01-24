d62ee925057243a037f329dce5cabc8c0039ee94
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-01-24 01:39:11 -0500

    add component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
-import FloorPlan from './FloorPlan';
+import ConstructorList from './ConstructorList';
+
 
 function App() {
+  const formulaOne = [
+  {
+    name: 'Mercedes',
+    engine: 'Mercedes',
+    championships: 8,
+    drivers: ['George Russell', 'Lewis Hamilton'] 
+  },
+  {
+    name: 'McLaren',
+    engine: 'Mercedes',
+    championships: 8,
+    drivers: ['Lando Norris', 'Oscar Paistri'] 
+  },
+  {
+    name: 'Mercedes',
+    engine: 'Mercedes',
+    championships: 8,
+    drivers: ['George Russell', 'Lewis Hamilton'] 
+  },
+  {
+    name: 'Redbull',
+    engine: 'Honda',
+    championships: 5,
+    drivers: ['Max Verstappen', 'Sergio Perez'] 
+  },
+  {
+    name: 'Ferrari',
+    engine: 'Ferrari',
+    championships: 16,
+    drivers: ['Charles Leclerc', 'Carlos Sainz'] 
+  }
+]
   return (
-    < >
-    <FloorPlan name= "FloorPlan"/>
-    </>
+    < div>
+    <ConstructorList />
+    </div>
   );
 }
 
---
+++ src/ConstructorList.jsx
@@ -0,0 +1,9 @@
+const ConstructorList = () => {
+  return ( 
+  < >
+    <h1> This is a F1 List</h1>
+  </>
+ );
+}
+
+export default  ConstructorList;
